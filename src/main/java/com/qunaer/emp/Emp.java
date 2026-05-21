0fc987acc140711ae2ad9e96cd96464809a692cc
Jason Zheng <jason.zheng0115@gmail.com>
2026-05-21 14:48:55 +0800

    First commit

---
+++ src/main/java/com/qunaer/emp/Emp.java
@@ -0,0 +1,20 @@
+package com.qunaer.emp;
+
+public class Emp {
+	private Integer empNo;
+	private String empName;
+	
+	public Integer getEmpNo() {
+		return empNo;
+	}
+	public void setEmpNo(Integer empNo) {
+		this.empNo = empNo;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	
+}
